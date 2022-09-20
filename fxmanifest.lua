67ab7542b89fea2468ceff842b767fc349a91f1e
LosSantosProject <113925663+LosSantosProject@users.noreply.github.com>
2022-09-20 13:21:46 +0200

    LSP_Shopsystem V1

---
+++ fxmanifest.lua
@@ -0,0 +1,39 @@
+fx_version 'adamant'
+game 'gta5'
+description 'Shop system by: LSP Team'
+version '0.99.01'
+
+shared_scripts {
+    'config/shared.lua', 
+    '@es_extended/imports.lua'
+}
+
+client_scripts {
+    'config/client.lua',
+    'client/*.lua'
+}
+
+server_scripts {
+    '@mysql-async/lib/MySQL.lua',
+    'server/*.lua'
+}
+
+ui_page {
+	'client/html/index.html'
+}
+
+files {
+    'client/html/index.html',
+
+    'client/html/logo192.png',
+    'client/html/logo512.png',
+    'client/html/manifest.json',
+    'client/html/favicon.ico',
+    'client/html/asset-manifest.json',
+
+    'client/html/static/js/*.js',
+    'client/html/static/css/*.css',
+
+    'client/html/items/*.png',
+    'client/html/items/*.jpg',
+}
